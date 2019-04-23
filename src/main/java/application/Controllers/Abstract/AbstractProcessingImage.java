f53330884de2445829d3291d2c5a0deaacd35d7d
Krystian Guminski <krystian.guminsk@gmail.com>
2019-04-23 10:36:35 +0200

    Added HeightMapController

--- src/main/java/application/Controllers/Abstract/AbstractProcessingImage.java
+++ src/main/java/application/Controllers/Abstract/AbstractProcessingImage.java
@@ -1,15 +1,22 @@
 package application.Controllers.Abstract;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import lombok.Setter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.IOException;
 
 @Setter
 public abstract class AbstractProcessingImage {
+
+    private static final Logger logger = LogManager.getLogger(AbstractProcessingImage.class.getName());
 
     private Button selectImageButton;
 
@@ -30,6 +37,16 @@ public abstract class AbstractProcessingImage {
                 actionButton.setVisible(true);
             }
         });
+    }
+
+    protected void saveImage(String outputNameFile) {
+        File outputFile = new File(outputNameFile);
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(afterImageView.getImage(), null), "png", outputFile);
+            logger.info("The image has been saved");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
     }
 
 }
--- src/main/java/application/Controllers/HeightMapController.java
+++ src/main/java/application/Controllers/HeightMapController.java
@@ -4,15 +4,23 @@ import application.Controllers.Abstract.AbstractProcessingImage;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HeightMapController extends AbstractProcessingImage implements Initializable {
 
+    private static final Logger logger = LogManager.getLogger(HeightMapController.class.getName());
+
     @FXML
-    public Button selectImageButton;
+    private Button selectImageButton;
 
     @FXML
     private Button actionButton;
@@ -30,9 +38,56 @@ public class HeightMapController extends AbstractProcessingImage implements Init
         super.setBeforeImageView(beforeImageView);
         super.setAfterImageView(afterImageView);
         selectPicture();
+        logger.info("All components have been loaded successfully");
     }
 
-    public void showImage() {
+    public void selectImage() {
         selectPicture();
+    }
+
+    public void showResult() {
+        afterImageView.setImage(coloringImage());
+        saveImage("out");
+    }
+
+    private WritableImage coloringImage() {
+        Image image = beforeImageView.getImage();
+        Integer height = (int) image.getHeight();
+        Integer width = (int) image.getWidth();
+
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+
+            for (int j = 0; j < height; j++) {
+
+                double x = image.getPixelReader().getColor(i, j).getBrightness();
+
+                if (x < 0.15) {
+                    pixelWriter.setColor(i, j, Color.DARKBLUE);
+                }
+                if (x < 0.40 && x > 0.15) {
+                    pixelWriter.setColor(i, j, Color.BLUE);
+                }
+                if (x < 0.150 && x > 0.40) {
+                    pixelWriter.setColor(i, j, Color.WHEAT);
+                }
+                if (x < 0.250 && x > 0.150) {
+                    pixelWriter.setColor(i, j, Color.OLIVE);
+                }
+                if (x < 0.450 && x > 0.250) {
+                    pixelWriter.setColor(i, j, Color.DARKOLIVEGREEN);
+                }
+                if (x < 0.600 && x > 0.450) {
+                    pixelWriter.setColor(i, j, Color.SILVER);
+                }
+                if (x > 0.600) {
+                    pixelWriter.setColor(i, j, Color.WHITE);
+                }
+            }
+        }
+
+        return writableImage;
     }
 }
